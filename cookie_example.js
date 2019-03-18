64c4970557aab15ee41d605db9a8eb9b3710dde2
“yashtandon7” <yash.tandon@girnarsoft.com>
2019-03-18 17:30:35 +0530

    new file

---
+++ cookie_example.js
@@ -0,0 +1,40 @@
+
+let express = require('express'); 
+let cookieParser = require('cookie-parser'); 
+let app = express();
+app.use(cookieParser()); 
+
+app.get('/', function(req, res){ 
+     res.send('welcome to express app'); 
+}); 
+  
+let users = { 
+     name : "Yash", 
+     Age : "18"
+} 
+
+let user = { 
+     name : "Tandon", 
+     Age : "21"
+} 
+  
+app.get('/setuser', function(req, res){ 
+     res.cookie("YashData", users, {maxAge: 20000});
+     res.cookie("TandonData", user, {maxAge: 10000});
+     res.send('user data added to cookie'); 
+}); 
+  
+app.get('/getuser', function(req, res){ 
+     res.send(req.cookies); 
+}); 
+
+app.get('/remove', function(req, res){   
+     res.clearCookie('userData'); 
+     res.send('user logout successfully'); 
+}); 
+  
+app.listen(3000, function(err){ 
+     if(err) 
+     throw err; 
+     console.log('listening on port 3000'); 
+}); 
