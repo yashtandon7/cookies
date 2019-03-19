1d4451927ea2e7d9943065f332d41bd45f98b32a
“yashtandon7” <yash.tandon@girnarsoft.com>
2019-03-19 11:23:48 +0530

    new cookie

--- cookie_example.js
+++ cookie_example.js
@@ -20,6 +20,7 @@ let user = {
   
 app.get('/setuser', function(req, res){ 
      res.cookie("YashData", users, {domain:'nodejscookie.herokuapp.com',maxAge: 200000, path:'/getuser'});
+     res.cookie("Data", users, {domain:'localhost',maxAge: 200000});
      res.cookie("TandonData", user, {domain:'nodejscookie.herokuapp.com',maxAge: 200000, path:'/remove'});
      res.send('user data added to cookie'); 
 }); 
